53b14b9e3c7872be569e4f4c526efc16df74ec44
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-07-11 21:18:29 +0000

    added alacritty and nvim

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- <BS>-- By default, prettier will only be used for formatting
+-- if a prettier configuration file is found in the project.
+-- Set to `false` to always use prettier for supported filetypes.
+vim.g.lazyvim_prettier_needs_config = true
